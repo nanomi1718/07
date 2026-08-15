12c984907fa3d5b24de2aab21fd9dff5f4a6e6bc
nanomi-hannah1718 <hannah1718@sookmyung.ac.kr>
2026-08-16 01:40:43 +0900

    03

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
  #include <stdio.h> 
+int inc(int counter);
+
 int main(void) 
 { 
-	int i;
-	int temp = 1; 
-	for (i=0;i<5;i++) 
-	{ 
+
+int i=10; 
+	printf("before i=%d\n", i); 
+	i=inc(i); // 함수의변환값이용 , i= 없으면 변환안됨 
+	printf("after i=%d\n", i); 
 	
-		
-		printf("temp = %d\n", temp); 
-		temp++; 
-	} 
-	return 0; 
+
 }
-		
-	
+int inc(int counter) 
+{ 
+	counter++; 
+	return counter; 
+}
+
