8c1ff5a3242df95eea0c1fb607bd4f792531c2d7
nanomi-hannah1718 <hannah1718@sookmyung.ac.kr>
2026-08-16 01:35:01 +0900

    02

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) { 
-	int flag = 1;
-	int y;
-	while (flag !=0)
+ #include <stdio.h> 
+int main(void) 
+{ 
+	int i;
+	int temp = 1; 
+	for (i=0;i<5;i++) 
 	{ 
 	
-	y=3;
-	flag=0;
-	}
-	y=4;
-	return 0;
-} 
+		
+		printf("temp = %d\n", temp); 
+		temp++; 
+	} 
+	return 0; 
+}
+		
+	
